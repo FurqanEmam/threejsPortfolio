5ac5a7485e689cc4fbb6352cbac392e6d1c0ddfe
Furqan <furqanemam@gmail.com>
2025-05-17 16:21:55 +0600

    added new section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Cube from "./components/Cube";
 import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
 
 export default function App() {
   return (
     <main>
       <Hero />
+      <ShowcaseSection />
     </main>
   );
 }

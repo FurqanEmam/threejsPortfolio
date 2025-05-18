73d2c7e806e193e33d592a223b80ef90e97c3a96
Furqan <furqanemam@gmail.com>
2025-05-18 16:14:09 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Cube from "./components/Cube";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <main>
+      <NavBar />
       <Hero />
       <ShowcaseSection />
     </main>

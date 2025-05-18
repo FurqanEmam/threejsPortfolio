0d25aec13d9b9a8b123bfc3b548660eb88a6ba6d
Furqan <furqanemam@gmail.com>
2025-05-19 04:22:28 +0600

    expSec Logo glow added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Cube from "./components/Cube";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
+import { LogoSection } from "./components/LogoSection";
+import { FeatureCards } from "./sections/FeatureCards";
+import { ExperienceSection } from "./sections/ExperienceSection";
 
 export default function App() {
   return (
@@ -10,6 +13,9 @@ export default function App() {
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
+      <FeatureCards />
+      <ExperienceSection />
     </main>
   );
 }
